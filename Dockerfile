a32d9e90fefab20bf45bc14750f6fb352ef0d322
modem7 <modem7@gmail.com>
2023-12-13 00:25:09 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# syntax = docker/dockerfile:latest
+
+# build stage
+FROM node:18.19-alpine3.19 AS build-stage
+
+# Set environment variables for non-interactive npm installs
+ENV NPM_CONFIG_LOGLEVEL warn
+
+WORKDIR /app
+
+COPY --chown=node . .
+
+RUN yarn && yarn build
+
+# production stage
+FROM nginxinc/nginx-unprivileged:1.25.2-alpine AS production-stage
+
+USER root
+
+ARG UID=101
+ARG GID=101
+
+COPY --from=build-stage /app/out /usr/share/nginx/html/
+COPY --link --chmod=755 scripts/nginx/*.sh /docker-entrypoint.d/
+
+RUN chown $UID:0 /usr/share/nginx/html/index.html
+
+# COPY nginx.conf /etc/nginx/conf.d/default.conf
+USER $UID
+
+# Document what port is required
+EXPOSE 8080
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ pages/_app.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
+import '../css/fonts.css';
+import '../css/main.css';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Yet Another Christmas Countdown!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <meta name="apple-mobile-web-app-status-bar-style" content="#070026" />
+
+        <meta name="og:title" content="Yet Another Christmas Countdown!" />
+        <meta name="tiwtter:title" content="Yet Another Christmas Countdown!" />
+
+        <meta name="og:url" content="https://YetAnotherChristmasCountdown.com" />
+
+        <meta name="og:description" content="A Christmas Countdown with no ads or social media icons, only the countdown!" />
+        <meta name="description" content="A Christmas Countdown with no ads or social media icons, only the countdown!" />
+        <meta name="twitter:description" content="A Christmas Countdown with no ads or social media icons, only the countdown!" />
+
+        <meta name="twitter:image" content="https://yetanotherchristmascountdown.com/social-media-picture.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="og:image" content="https://yetanotherchristmascountdown.com/social-media-picture.jpg" />
+
+        <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer>{}</script>
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object, // eslint-disable-line
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
